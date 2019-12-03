d82e07cf2b38ef088db1ddba9336d97a8322416a
Richard Kuhnt <r15ch13+git@gmail.com>
2019-12-03 20:37:12 +0100

    Add TTL flag

--- index.php
+++ index.php
@@ -7,12 +7,12 @@ use \Cloudflare\API\Endpoints\Zones as CloudflareZones;
 use \Cloudflare\API\Endpoints\DNS as CloudflareDNS;
 header('Content-Type: text/plain');
 
-function updateOrCreate(CloudflareDNS $dns, string $zoneID, $record, string $type, string $name, string $content)
+function updateOrCreate(CloudflareDNS $dns, string $zoneID, $record, string $type, string $name, string $content, int $ttl = 120)
 {
   if (!empty($record)) {
-    return $dns->updateRecordDetails($zoneID, $record->id, ['type' => $type, 'name' => $name, 'content' => $content, 'ttl' => 120]);
+    return $dns->updateRecordDetails($zoneID, $record->id, ['type' => $type, 'name' => $name, 'content' => $content, 'ttl' => $ttl]);
   } else {
-    return $dns->addRecord($zoneID, $type, $name, $content, 120, false);
+    return $dns->addRecord($zoneID, $type, $name, $content, $ttl, false);
   }
 }
 
@@ -24,6 +24,7 @@ $zone = (string)$request->get('zone');
 $domain = (string)$request->get('domain');
 $ipv4 = (string)$request->get('ipv4', (string)$request->get('ip', (string)$request->getClientIp()));
 $ipv6 = (string)$request->get('ipv6');
+$ttl = (int)$request->get('ttl', 120);
 
 // Cloudflare API Headers
 if (empty($email) || empty($key)) {
@@ -47,7 +48,8 @@ if (empty($email) || empty($key) || empty($zone) || empty($domain)) {
   echo "domain = Domain or Subdomain\n";
   echo "wildcard = Use *.example.org\n";
   echo "ip|ipv4 = Update ipv4 (no auto detection)\n";
-  echo "ipv6 = Update ipv6\n\n";
+  echo "ipv6 = Update ipv6\n";
+  echo "ttl = Update TTL value (default 120 sec)\n\n";
   echo "The username and password field of your FRITZ!Box settings will be used.\n\n";
   echo "FRITZ!Box Example: https://$host/?zone=example.org&domain=<domain>&ipv4=<ipaddr>&ipv6=<ip6addr>";
   return;
@@ -60,26 +62,26 @@ $dns = new CloudflareDNS($adapter);
 $zoneID = $zones->getZoneID($zone);
 
 $record = reset($dns->listRecords($zoneID, 'A', $domain)->result);
-$result = updateOrCreate($dns, $zoneID, $record, 'A', $domain, $ipv4);
+$result = updateOrCreate($dns, $zoneID, $record, 'A', $domain, $ipv4, $ttl);
 echo "Updated $domain to $ipv4" . PHP_EOL;
 if(!$result) { print_r($result); }
 
 if (!is_null($request->get('wildcard'))) {
   $wildcard = reset($dns->listRecords($zoneID, 'A', '*.' . $domain)->result);
-  $result = updateOrCreate($dns, $zoneID, $wildcard, 'A', '*.' . $domain, $ipv4);
+  $result = updateOrCreate($dns, $zoneID, $wildcard, 'A', '*.' . $domain, $ipv4, $ttl);
   echo "Updated *.$domain to $ipv4";
   if(!$result) { print_r($result); }
 }
 
 if ($ipv6) {
   $record = reset($dns->listRecords($zoneID, 'AAAA', $domain)->result);
-  $result = updateOrCreate($dns, $zoneID, $record, 'AAAA', $domain, $ipv6);
+  $result = updateOrCreate($dns, $zoneID, $record, 'AAAA', $domain, $ipv6, $ttl);
   echo "Updated $domain to $ipv6" . PHP_EOL;
   if(!$result) { print_r($result); }
 
   if (!is_null($request->get('wildcard'))) {
     $wildcard = reset($dns->listRecords($zoneID, 'AAAA', '*.' . $domain)->result);
-    $result = updateOrCreate($dns, $zoneID, $wildcard, 'AAAA', '*.' . $domain, $ipv6);
+    $result = updateOrCreate($dns, $zoneID, $wildcard, 'AAAA', '*.' . $domain, $ipv6, $ttl);
     echo "Updated *.$domain to $ipv6";
     if(!$result) { print_r($result); }
   }
